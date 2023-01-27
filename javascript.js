f422c390a45c37177cf921b19df4bd0504dae221
Ian Santana <thief.jones@gmail.com>
2023-01-27 11:39:09 +0000

    avance_2

--- javascript.js
+++ javascript.js
@@ -1,25 +1,51 @@
+let boton_origen;
 
-function abrirventana1(url) {
+function abrirventana1(url, boton) {
     let name_ventana = url.substring(0,8);
+    let boton_ = document.getElementById(boton);
+    boton_origen = boton_.innerHTML;
+    alert(boton_origen);
 
-    ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+    let ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+    $(ventana1.document).load(function() {
+        alert('loaded');
+        // do other things
+    });
+    boton_origen = boton_.innerHTML;
+    alert(boton_origen);
 }
 
 function abrirventana2(url1,url2,url3) {
-    alert("rbgwg");
-    alert(url2);
-    let name_ventana1 = url1.substring(0,8);
-    let name_ventana2 = url2.substring(0,8);
-    let name_ventana3 = url3.substring(0,8);
+    let boton_ = document.getElementById(boton);
+    boton_origen = boton_.innerHTML;
 
+    ventana1 = window.open(url1);
+    ventana2 = window.open(url2);
+    ventana3 = window.open(url3);
+}
 
-    ventana1 = window.open(url1, name_ventana1, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
-    ventana2 = window.open(url2, name_ventana2, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
-    ventana3 = window.open(url3, name_ventana3, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+function abrirventana3(url, boton) {
+    let name_ventana = url.substring(0,8);
+    let boton_ = document.getElementById(boton);
+    boton_origen = boton_.innerHTML;
+    ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, resizable=si, tittlebar=no, width=400, height=400");
+
+}
+
+function abrirventana4(url, boton) {
+    let name_ventana = url.substring(0,8);
+    let boton_ = document.getElementById(boton);
+    boton_origen = boton_.innerHTML;
+    ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
 }
 
 function nombre(){
     alert("El nombre de la ventana es " + document.title);
+}
+
+function quien_me_creo(){
+    alert(boton_origen)
+    alert('Esta ventana fue creada por el boton: ' + boton_origen);
 }
 
 function cerrar() {
