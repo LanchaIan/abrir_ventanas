1850eac27afdb90cdc9b6ff7dd651214db3ac7e7
Ian Santana <thief.jones@gmail.com>
2023-01-30 12:30:29 +0000

    multiples_ventanas_funciona

--- javascript.js
+++ javascript.js
@@ -1,27 +1,26 @@
 let boton_origen;
 
-function abrirventana1(url, boton) {
-    let name_ventana = url.substring(0,8);
-    let boton_ = document.getElementById(boton);
-    boton_origen = boton_.innerHTML;
-    alert(boton_origen);
+window.onload = function(){
+    if (document.title == "Ventana 2") {
+        ventana3 = window.open("ventana3.html", "Ventana 3", "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
 
+    }
+    else if (document.title == "Ventana 3") {
+        ventana4 = window.open("ventana4.html", "Ventana 4", "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+
+    }
+  };
+
+function abrirventana1(url) {
+    let name_ventana = url.substring(0,8);
     let ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
-    $(ventana1.document).load(function() {
-        alert('loaded');
-        // do other things
-    });
-    boton_origen = boton_.innerHTML;
-    alert(boton_origen);
 }
 
-function abrirventana2(url1,url2,url3) {
-    let boton_ = document.getElementById(boton);
-    boton_origen = boton_.innerHTML;
+function abrirventana2(url) {
+    let name_ventana = url.substring(0,8);
 
-    ventana1 = window.open(url1);
-    ventana2 = window.open(url2);
-    ventana3 = window.open(url3);
+    ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+
 }
 
 function abrirventana3(url, boton) {
