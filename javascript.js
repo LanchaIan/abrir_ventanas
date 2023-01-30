b3fdf90b573a46fbb0938e3fe6cc04b7a4ac1ab6
Ian Santana <thief.jones@gmail.com>
2023-01-30 13:43:33 +0000

    casi terminado

--- javascript.js
+++ javascript.js
@@ -1,25 +1,52 @@
 let boton_origen;
-
+let ventana1;
+let ventanas;
+let ventana2;
+let ventana3;
+let ventana4;
+let ventana5;
 window.onload = function(){
-    if (document.title == "Ventana 2") {
+    alert(boton_origen);
+    if (document.title == "Ventana 1") {
         ventana3 = window.open("ventana3.html", "Ventana 3", "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+        boton_origen = "ABRIR VENTANA SIN CARACTERÍSTICAS UNA SOLA VEZ";
+
+    }
+    else if (document.title == "Ventana 2") {
+        ventana3 = window.open("ventana3.html", "Ventana 3", "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+        boton_origen = "ABRIR VENTANA SIN CARACTERÍSTICAS VARIAS VECES";
 
     }
     else if (document.title == "Ventana 3") {
         ventana4 = window.open("ventana4.html", "Ventana 4", "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
-
+        boton_origen = "ABRIR VENTANA SIN CARACTERÍSTICAS VARIAS VECES";
+    }
+    else if (document.title == "Ventana 4") {
+        boton_origen = "ABRIR VENTANA SIN CARACTERÍSTICAS VARIAS VECES";
+    }
+    else if (document.title == "Ventana 5") {
+        boton_origen = "ABRIR VENTANA CON CARACTERÍSTICAS";
+    }
+    else if (document.title == "Ventana 6") {
+        boton_origen = "boton 4";
+    }
+    else if (document.title == "Ventana 7") {
+        boton_origen = "boton 5";
     }
   };
 
-function abrirventana1(url) {
+function abrirventana1(url, boton) {
     let name_ventana = url.substring(0,8);
-    let ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+    let boton_ = document.getElementById(boton);
+    boton_origen = boton_.innerHTML;
+    ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
 }
 
-function abrirventana2(url) {
+function abrirventana2(url, boton) {
     let name_ventana = url.substring(0,8);
-
-    ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+    let boton_ = document.getElementById(boton);
+    boton_origen = boton_.innerHTML;
+    ventanas = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
 
 }
 
@@ -27,7 +54,7 @@ function abrirventana3(url, boton) {
     let name_ventana = url.substring(0,8);
     let boton_ = document.getElementById(boton);
     boton_origen = boton_.innerHTML;
-    ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, resizable=si, tittlebar=no, width=400, height=400");
+    ventana3 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=yes, resizable=yes, tittlebar=no, width=400, height=400");
 
 }
 
@@ -35,7 +62,16 @@ function abrirventana4(url, boton) {
     let name_ventana = url.substring(0,8);
     let boton_ = document.getElementById(boton);
     boton_origen = boton_.innerHTML;
-    ventana1 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no");
+    ventana4 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no, resizable=yes, width=600, height=300, left=150");
+
+}
+
+function abrirventana5(url, boton) {
+    let name_ventana = url.substring(0,8);
+    let boton_ = document.getElementById(boton);
+    boton_origen = boton_.innerHTML;
+    ventana5 = window.open(url, name_ventana, "directories=no, location=no, menubar=no, scrollbars=no, statusbar=no, tittlebar=no, fullscreen='yes'");
+
 }
 
 function nombre(){
@@ -49,6 +85,15 @@ function quien_me_creo(){
 
 function cerrar() {
     window.close();
+}
+
+function cerrar_todo() {
+    ventana1.close();
+    ventanas.close();
+    ventana2.close();
+    ventana3.close();
+    ventana4.close();
+    ventana5.close();
 }
 
 function entra(botonx){
